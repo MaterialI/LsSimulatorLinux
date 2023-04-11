dc4ea65938daf6c6ad5daa52e520331fbebebd46
sra124 <sra124@sfu.ca>
2023-04-11 09:48:01 -0700

    pulled

--- main.c
+++ main.c
@@ -1,3 +1,164 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <dirent.h>
+// #include <sys/stat.h>
+// #include <string.h>
+// #include <time.h>
+// #include <pwd.h>
+// #include <grp.h>
+// #include <unistd.h>
+
+// static char* test = "/home/vagrant/Documents/CMPT276/project/parabix-devel-master";
+// //static char* test = "./Test";
+// // Disc : Opens  a directory for reading and reutnrs null if the file doesn't exist 
+
+
+// typedef void (*flag_func)(DIR*);
+
+// DIR* open_Dir(char* dir_path){
+//         DIR* dir = opendir(dir_path);
+//     if(!dir){return NULL;}
+//     return dir;
+// }
+
+
+// //Disc : Reads the contents of a file 
+// void name_Read_Entities(DIR* dir ){
+//     struct dirent* file_entity;
+//     file_entity = readdir(dir);
+
+//      while (file_entity != NULL)
+//     { 
+//         if(strcmp(file_entity->d_name , ".") != 0  && strcmp(file_entity->d_name , "..") != 0 ) 
+//         {  
+//             printf("%s\n",   file_entity->d_name );
+//         }
+        
+//         file_entity = readdir(dir);
+//     }
+
+// }
+
+
+
+
+
+// //
+// void indoe_Read_Entities(DIR* dir){
+//     int i = 1; 
+//     struct dirent* file_entity;
+//     file_entity = readdir(dir);
+//      while (file_entity != NULL)
+//     {   if(strcmp(file_entity->d_name , ".") != 0  && strcmp(file_entity->d_name , "..") != 0 ) 
+//     {  
+       
+//         printf(" %8lu   %-8s  ",  file_entity->d_ino, file_entity->d_name );
+//          if(i%4 == 0){printf("\n\n" );}
+//          i++;
+
+    
+//     }
+        
+//         file_entity = readdir(dir);
+//     }
+//    printf("\n");
+// }
+
+// void L_Read_Entities(DIR* dir)
+// {
+//     struct dirent* file_entity;
+//     file_entity = readdir(dir);
+
+//     struct stat s;
+//      while (file_entity != NULL)
+//     {   if((strcmp(file_entity->d_name , "..") && strcmp(file_entity->d_name , ".")  ))
+//         {   
+//             stat(file_entity->d_name , &s);
+//              printf(" File ID is : %lu  File name is : %s\n ", s.st_ino  , file_entity->d_name);
+//             file_entity = readdir(dir);
+//         }
+//         else {break;}
+//     }
+// }
+// void R_Read_Entities(DIR* dir, char* aPath, flag_func passedFunc){
+
+//     struct dirent* file_entity;
+//     printf("%s\n", aPath);
+//     passedFunc(dir);
+//     dir = open_Dir(aPath);
+//     file_entity = readdir(dir);
+    
+//      while (file_entity != NULL)
+//     {   
+//         if((strcmp(file_entity->d_name , "..") && strcmp(file_entity->d_name , ".")  ))
+//         {
+//              char path[300] = {0};
+//             if(file_entity->d_type == 4){
+//                 strcat(path, aPath);
+//                 strcat(path, "/");
+//                 strcat(path, file_entity->d_name);
+//                 DIR* dr = opendir(path);
+//                 printf("\n");
+//                 R_Read_Entities(dr, path,passedFunc);
+//             }  
+//         }
+//         file_entity = readdir(dir);
+//     }
+
+// }
+
+
+// //Desc opens the given directory and reads it's contents  
+// // returns -1 if the file DNE 
+
+// int ls_Default(char* dir_path){
+    
+//    DIR* dir =  opendir(dir_path);
+
+//     if(dir == NULL){printf("Bad Input : Cannot open the file \n"); return -1;}
+//     name_Read_Entities(dir);
+// }
+
+// int l_Flag(char* dir_path)
+// {
+//     DIR* dir =  opendir(dir_path);
+//     if(dir == NULL){printf("Bad Input : Cannot open the file \n"); return -1;}
+//     L_Read_Entities(dir);
+
+// }
+
+
+
+// //Desc opens the given directory and reads it's contents  
+// // returns -1 if the file DNE 
+
+// int i_Flag(char* dir_path){
+    
+//    DIR* dir =  opendir(dir_path);
+
+//     if(dir == NULL){printf("Bad Input : Cannot open the file \n"); return -1;}
+//     indoe_Read_Entities(dir);
+// }
+// int R_Flag(char* dir_path, flag_func passedFunc){
+    
+//    DIR* dir =  opendir(dir_path);
+
+//     if(dir == NULL)
+//     {printf("Bad Input : Cannot open the file \n"); return -1;}
+//     R_Read_Entities(dir, test, passedFunc);
+// }
+
+
+
+// int main(int argc, char ** argv) {
+
+
+
+//     R_Flag(test, name_Read_Entities);
+
+
+//     return 0 ; 
+// }
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
